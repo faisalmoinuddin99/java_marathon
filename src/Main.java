9c5c0be1b81c998dec648cce1fd964a94c751e88
faisalmoinuddin99 <faisal25march99@gmail.com>
2022-08-02 07:01:19 +0530

    Abstraction Example

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,46 @@
+class Shape {
 
+   private String name ;
+   private int length ;
+   private int breadth ;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public int getBreadth() {
+        return breadth;
+    }
+
+    public void setBreadth(int breadth) {
+        this.breadth = breadth;
+    }
+}
+class  Rectangle extends Shape {
+
+    int area(int length, int breadth) {
+        return  length * breadth ;
+    }
+}
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+
+        Rectangle r1 = new Rectangle() ;
+        r1.setLength(10) ;
+        r1.setBreadth(20) ;
+        int areaOfRectangle = r1.area(r1.getLength(),r1.getBreadth()) ;
+        System.out.println(areaOfRectangle); // 200
     }
 }
